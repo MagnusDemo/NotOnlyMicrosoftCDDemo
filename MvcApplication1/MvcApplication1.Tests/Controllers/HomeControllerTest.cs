43bea929ab15786910d605a3bacc9bca1e6ab135
Magnus Mårtensson <himself@noop.se>
2012-11-10 19:35:50 +0100

    Fixed a test.

--- MvcApplication1/MvcApplication1.Tests/Controllers/HomeControllerTest.cs
+++ MvcApplication1/MvcApplication1.Tests/Controllers/HomeControllerTest.cs
@@ -17,7 +17,7 @@ namespace MvcApplication1.Tests.Controllers
 			var result = controller.Index() as ViewResult;
 
 			// Assert
-			Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+			Assert.AreEqual(null, result.ViewBag.Message);
 		}
 	}
 }
