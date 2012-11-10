865be98349d9caf721b8e33c50df554b17ff74dd
Magnus Mårtensson <himself@noop.se>
2012-11-10 19:30:58 +0100

    Updated the home page message and cleaned up the proejct.

--- MvcApplication1/MvcApplication1.Tests/Controllers/HomeControllerTest.cs
+++ MvcApplication1/MvcApplication1.Tests/Controllers/HomeControllerTest.cs
@@ -11,39 +11,13 @@ namespace MvcApplication1.Tests.Controllers
 		public void Index()
 		{
 			// Arrange
-			HomeController controller = new HomeController();
+			var controller = new HomeController();
 
 			// Act
-			ViewResult result = controller.Index() as ViewResult;
+			var result = controller.Index() as ViewResult;
 
 			// Assert
 			Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
-		}
-
-		[Test]
-		public void About()
-		{
-			// Arrange
-			HomeController controller = new HomeController();
-
-			// Act
-			ViewResult result = controller.About() as ViewResult;
-
-			// Assert
-			Assert.IsNotNull(result);
-		}
-
-		[Test]
-		public void Contact()
-		{
-			// Arrange
-			HomeController controller = new HomeController();
-
-			// Act
-			ViewResult result = controller.Contact() as ViewResult;
-
-			// Assert
-			Assert.IsNotNull(result);
 		}
 	}
 }
